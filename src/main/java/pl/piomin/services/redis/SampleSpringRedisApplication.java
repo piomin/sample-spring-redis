fea2e5a9013409cb69ab2deca22588c423bb4388
minkowp <piotr.minkowski@play.pl>
2019-03-05 14:29:57 +0100

    removing unnecessary configuration

--- src/main/java/pl/piomin/services/redis/SampleSpringRedisApplication.java
+++ src/main/java/pl/piomin/services/redis/SampleSpringRedisApplication.java
@@ -2,8 +2,10 @@ package pl.piomin.services.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @SpringBootApplication
+@EnableRedisRepositories
 public class SampleSpringRedisApplication {
 
     public static void main(String[] args) {
